1c516f8d3fe599fa4f95aadd704a8f10cccc4ff0
pmaques <paola.marquesserra2@gamil.com>
2022-09-21 14:42:38 -0300

    primer commit

---
+++ portfolioangular/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { LogrosComponent } from './componentes/logros/logros.component';
+import { PortfolioService } from './servicios/portfolio.service';
+import {HttpClientModule} from '@angular/common/http'
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    AptitudesComponent,
+    LogrosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
